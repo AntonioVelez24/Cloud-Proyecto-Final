0f1d9b62df4d1d2d2356c6ab676ce45a40935767
AntonioVelez24 <fabricio.velez@tecsup.edu.pe>
2025-03-31 17:10:01 -0500

    Proccessing

---
+++ src/common/MainHeader.tsx
@@ -0,0 +1,12 @@
+//import './MainHeader.css'
+function MainHeader() {
+    return (
+        <header id="main-header">
+            <div className='container'>
+                <h1>Fabricio Antonio Velez Bazán</h1>
+                <p>Desarrollo de Videojuegos en Soluciones Cloud</p>
+            </div>
+        </header>
+    )
+}
+export default MainHeader
